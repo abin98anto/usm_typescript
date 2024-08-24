2f012b45dce244ea1d2125bf538411000fc2801b
Abin Anto <106867862+abin98anto@users.noreply.github.com>
2024-08-24 09:14:11 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import path from "path";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", [
+  path.join(__dirname, "views", "users"),
+  path.join(__dirname, "views", "admin"),
+  path.join(__dirname, "views", "partials"),
+]);
+
+app.get("/", (req, res) => res.render("sign_up"));
+app.get("/login", (req, res) => res.render("user_login"));
+
+app.listen(3000, () => console.log("server started : http://localhost:3000/"));
