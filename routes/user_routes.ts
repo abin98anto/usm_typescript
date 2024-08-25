1cc51c2cf5392e0608c9ce4bfb0d1231587c9209
Abin Anto <106867862+abin98anto@users.noreply.github.com>
2024-08-25 15:21:27 +0530

    user_login.ejs issue cleared with teh error message

--- routes/user_routes.ts
+++ routes/user_routes.ts
@@ -5,19 +5,22 @@ import AuthMiddleware from "../middlewares/auth";
 const router = Router();
 
 router.get("/", AuthMiddleware.is_loggedout, UserController.render_signup);
+router.post("/", UserController.create_user);
+
 router.get(
   "/login",
   AuthMiddleware.is_loggedout,
   UserController.render_user_login
 );
+router.post("/login", UserController.verify_login);
+
 router.get(
   "/dashboard",
   AuthMiddleware.is_loggedin,
   UserController.render_user_dashboard
 );
+
 router.post("/check-email", UserController.check_email);
-router.post("/", UserController.create_user);
-router.post("/login", UserController.verify_login);
 router.post("/logout", UserController.user_logout);
 
 export default router;
